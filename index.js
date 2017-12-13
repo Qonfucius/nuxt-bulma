ffcb04d055d9ed16212b60d1952fd9651f42cff5
Gaël <gael@hungryup.co>
2017-12-13 15:26:44 +0100

    add postcss option

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-module.exports = function nuxtBulma({ css = true } = {}) {
+module.exports = function nuxtBulma({ css = true, postcss = true } = {}) {
   if (css) {
     this.options.css.push('bulma/css/bulma.css');
+  }
+  if (postcss) {
+    Object.assign(
+      this.options.build,
+      {
+        postcss: {
+          plugins: {
+            'postcss-cssnext': {
+              features: {
+                customProperties: false,
+              },
+            },
+          },
+        },
+      }
+    );
   }
 };
 
