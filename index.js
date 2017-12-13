087f7c3cf8d73d2549c03fcaa8a21f4f85998cc8
Gaël <gael@hungryup.co>
2017-12-13 15:49:34 +0100

    Simplify set thanks to lodash

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
+const set = require('lodash.set');
+
 module.exports = function nuxtBulma({ css = true, postcss = true } = {}) {
   if (css) {
     this.options.css.push('bulma/css/bulma.css');
   }
   if (postcss) {
-    this.options.build = Object.assign(
-      this.options.build || {},
-      {
-        postcss: {
-          plugins: {
-            'postcss-cssnext': {
-              features: {
-                customProperties: false,
-              },
-            },
-          },
-        },
-      }
-    );
+    set(this.options, ['build', 'postcss', 'plugins', 'postcss-cssnext', 'features', 'customProperties'], false)
   }
 };
 
